e9d3fe68205a87f9628c256989a1523cd261fbd1
dihoon <dihoon955@gmail.com>
2023-01-13 04:35:30 +0900

    Feat: 배너 play 버튼 구현

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const myAxios = axios.create({
   baseURL : "https://api.themoviedb.org/3",
   params: {
     api_key: "634517f24799fc979ef074f28366b115",
@@ -8,4 +8,4 @@ const instance = axios.create({
   },
 })
 
-export default instance;
+export default myAxios;
