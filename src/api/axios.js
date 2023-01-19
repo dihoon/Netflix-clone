81cc1642cd3190f873d0e477bf5875d141d4e19d
dihoon <dihoon955@gmail.com>
2023-01-20 07:49:42 +0900

    env 파일 세팅

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const myAxios = axios.create({
   baseURL : "https://api.themoviedb.org/3",
   params: {
-    api_key: "634517f24799fc979ef074f28366b115",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 })
